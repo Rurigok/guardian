48067b701c3f7bc834689047424f2d68e8122cb2
Andrew Sanetra <andrew@acm-utsa.org>
2016-07-22 00:29:16 -0400

    Create initial firewalld actions interface

---
+++ guardiand/actions/firewalld.py
@@ -0,0 +1,25 @@
+from guardiand.logger.logger import Logger
+
+class FirewalldActions(object):
+    """
+    """
+
+    logger = Logger('firewalld-actions')
+
+    def __init__(self):
+        # TODO: implement if we don't use static methods
+        pass
+
+    @staticmethod
+    def ban_ip(ip_address):
+        """
+        """
+        # TODO: implement
+        return False
+
+    @staticmethod
+    def unban_ip(ip_address):
+        """
+        """
+        # TODO: implement
+        return False
