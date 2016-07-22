e2f9ebeeef07eb31032cce1c185b9c26919c3294
Andrew Sanetra <asanetras@gmail.com>
2016-07-22 07:25:48 -0400

    Add new IP Tables action class

---
+++ guardiand/actions/iptables.py
@@ -0,0 +1,25 @@
+from guardiand.logger.logger import Logger
+
+class IPTablesActions(object):
+    """
+    """
+
+    logger = Logger('iptables-actions')
+
+    def __init__(self):
+        # TODO: implement if we don't use static methods
+        pass
+
+    @staticmethod
+    def ban_ip(ip_address):
+        """
+        """
+        # TODO: implement
+        return False
+
+    @staticmethod
+    def unban_ip(ip_address):
+        """
+        """
+        # TODO: implement
+        return False
